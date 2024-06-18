1facfc2aef342465743e0a775059a9868b69c22e
gvirus21 <gouravkumar21.dev@gmail.com>
2024-06-18 19:14:58 +0530

    implemented parallax image

--- src/app/_sections/hero-section-bottom.tsx
+++ src/app/_sections/hero-section-bottom.tsx
@@ -18,10 +18,13 @@ const HeroSectionBottom = ({ scrollYProgress }: Props) => {
   const scale = useTransform(scrollYProgress, [0, 0.25], [0.8, 1]);
   const rotate = useTransform(scrollYProgress, [0, 0.25], [-5, 0]);
 
-  const { scrollYProgress: sliderScreenYProgress } = useScroll({
+  const { scrollYProgress: containerScrollYProgress } = useScroll({
     target: container,
     offset: ["start end", "end start"],
   });
+
+  const smParralax = useTransform(containerScrollYProgress, [0, 1], [-100, 0]);
+  const mdParralax = useTransform(containerScrollYProgress, [0, 1], [0, -350]);
 
   return (
     <motion.section
@@ -29,6 +32,7 @@ const HeroSectionBottom = ({ scrollYProgress }: Props) => {
         scale,
         rotate,
       }}
+      ref={container}
       className="relative flex justify-center items-center h-screen w-screen max-w-full bg-slate-200 text-black overflow-x-hidden font-mono gallery-clip"
     >
       <div className="flex flex-col justify-between h-screen w-screen py-20">
@@ -36,24 +40,53 @@ const HeroSectionBottom = ({ scrollYProgress }: Props) => {
           text="PROFESSIONAL PHOTOSHOOT"
           left="-80%"
           direction="right"
-          progress={sliderScreenYProgress}
+          progress={containerScrollYProgress}
         />
         <Slider
           text="ASTHETIC VIDEOSHOOT"
           left="-50%"
-          direction="right"
-          progress={sliderScreenYProgress}
+          direction="left"
+          progress={containerScrollYProgress}
         />
       </div>
-      <div className="absolute top-1/2 -translate-y-1/2 h-[30rem] aspect-[3/4] overflow-hidden bg-pink-300">
-        <Image
-          className="h-[120%] w-[120%] object-cover"
-          src={"/images/sideface-model.jpg"}
-          width={500}
-          height={100}
-          alt="model"
-        />
-      </div>
+      {/* images */}
+      <>
+        <motion.div
+          style={{ y: smParralax }}
+          className="absolute top-[50%] left-[36%] h-[16rem] aspect-[3/4] bg-pink-500 z-10 shadow-xl"
+        >
+          <Image
+            className="h-[120%] w-[120%] object-cover"
+            src={"/images/model-1.jpg"}
+            width={500}
+            height={100}
+            alt="model"
+          />
+        </motion.div>
+
+        <motion.div className="absolute top-1/2 -translate-y-1/2 h-[40rem] aspect-[3/4] overflow-hidden bg-pink-300 shadow-xl">
+          <Image
+            className="h-[120%] w-[120%] object-cover"
+            src={"/images/model-2.jpg"}
+            width={500}
+            height={100}
+            alt="model"
+          />
+        </motion.div>
+
+        <motion.div
+          style={{ y: mdParralax }}
+          className="absolute top-2/4 -translate-y-1/2 right-[32%] h-[24rem] aspect-[3/4] bg-pink-500 shadow-xl"
+        >
+          <Image
+            className="h-[120%] w-[120%] object-cover"
+            src={"/images/model-3.webp"}
+            width={500}
+            height={100}
+            alt="model"
+          />
+        </motion.div>
+      </>
     </motion.section>
   );
 };
